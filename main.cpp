885dacb2cda44f5df3db6f4675fb10307111b3d0
Emilio Barreiro <ebarr006@ucr.edu>
2016-09-06 13:01:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -39,19 +39,3 @@ int main() {
 
   return 0;
 }
-
-// refering to maze.cpp -> moveMouse()
-//    - in regards to checking bot wall, MUST make sure that when generating
-//      the maze initially, set corresponding adjacent cell's walls after
-//      setting a wall. Ex. if i set maze[3][3]'s bottom wall, i must make sure
-//      to set maze[4][3]'s top wall. displayMaze() only references top and left
-//      walls, so printing ~should~ be unaffected.
-
-
-// WILL HAVE TO HAVE 2 SEPERATE MAZES, the unknown maze (walls already set)
-// and the other being the printed terminal maze. adjust CheckPeripherals
-// the if statement should check the invisible maze, the subsequent code should
-// set the resulting walls for the displayed maze
-
-// ACCOUNT FOR DEAD-ENDS, WHERE isTop(), isLeft(), isRight() ALL RETURN (1);
-// MUST U-TURN;
